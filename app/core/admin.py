bfb2f2118160c095ddbd0c0dc0ac84fd8c0d7fe7
Pierre <pierre.schade@campus.tu-berlin.de>
2021-01-17 16:58:32 +0100

    merge fixes

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,18 +1,12 @@
-# Register your models here.
 from django.contrib import admin
 
-<<<<<<< HEAD
 # Register your models here.
-from .models import Vehicle, VehicleType, WorkshopCategory, Company, Tender
+from core.models import Vehicle, VehicleType, WorkshopCategory, Company, Tender, Route
 
 admin.site.register(Vehicle)
 admin.site.register(VehicleType)
 admin.site.register(WorkshopCategory)
-admin.site.register(Company)
 admin.site.register(Tender)
-=======
-from core.models import Company, Route
-
 
 class CompanyAdmin(admin.ModelAdmin):
     # fieldsets = [
@@ -24,13 +18,9 @@ class CompanyAdmin(admin.ModelAdmin):
     def get_owners(self, obj):
         return "".join([u.username for u in obj.ownership.all()])
 
-
 admin.site.register(Company, CompanyAdmin)
-
 
 class RouteAdmin(admin.ModelAdmin):
     list_display = ('name', 'operator', 'type', 'revenue', 'start_date', 'end_date')
 
-
 admin.site.register(Route)
->>>>>>> 933c05833a1daa8d2508d15f94ed9309f3141ea3
